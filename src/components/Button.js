3cc5be70f361d9e78a8e0a7759cd61b36c7cc293
Leonam Quintão <leonamquintao@gmail.com>
2018-11-20 21:22:16 -0200

    include Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+/* simple level component */
+const Button = (props) => {
+  return (
+    <TouchableOpacity style={ styles.buttonStyle } onPress={ props.propMethod }>
+      <Text style={ styles.buttonTxt }> { props.buttonText } </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  buttonTxt: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10,
+  }
+});
+
+export default Button;
