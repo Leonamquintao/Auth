f7e058b76f19afac60ec8dbe08821535d7abb95f
Leonam Quintão <leonamquintao@gmail.com>
2018-11-22 22:17:08 -0200

    add .env

--- App.js
+++ App.js
@@ -9,14 +9,15 @@ import LoginForm from './src/components/LoginForm';
 export default class App extends Component<Props> {
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: 'AIzaSyAPPseB6m_XVD-G6kGssf69bbdbC7wXIW8',
-      authDomain: 'auth-react-123.firebaseapp.com',
-      databaseURL: 'https://auth-react-123.firebaseio.com',
-      projectId: 'auth-react-123',
-      storageBucket: 'auth-react-123.appspot.com',
-      messagingSenderId: '436398464697'
+      apiKey: API_KEY,
+      authDomain: AUTH_DOMAIN,
+      databaseURL: DATABASE_URL,
+      projectId: PROJECT_ID,
+      storageBucket: STORAGE_BUCKET,
+      messagingSenderId: MESSAGING_SENDER_ID
     });
   }
+  
   render() {
     return (
       <View style={{ flex: 1 }}>
